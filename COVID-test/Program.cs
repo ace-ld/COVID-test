0a206a808a6e729f83310a5f97a40557302bed08
ace-ld <54129812+ace-ld@users.noreply.github.com>
2020-10-06 14:50:18 +0300

    clean code

--- COVID-test/Program.cs
+++ COVID-test/Program.cs
@@ -1,9 +1,7 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Net.Http;
 using System.Net.Http.Headers;
 using System.Text.Json;
-using System.Text.Json.Serialization;
 using System.Threading.Tasks;
 
 namespace COVID_test
