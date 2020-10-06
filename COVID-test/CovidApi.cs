01bd33773de3b47b0a72ff1788b731628dc05d3e
ace-ld <54129812+ace-ld@users.noreply.github.com>
2020-10-06 14:33:37 +0300

    Add project files.

---
+++ COVID-test/CovidApi.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text.Json.Serialization;
+
+namespace COVID_test
+{
+    public class Global
+    {
+        public int NewConfirmed { get; set; }
+        public int TotalConfirmed { get; set; }
+        public int NewDeaths { get; set; }
+        public int TotalDeaths { get; set; }
+        public int NewRecovered { get; set; }
+        public int TotalRecovered { get; set; }
+    }
+
+    public class Country
+    {
+        [JsonPropertyName("Country")]
+        public string CountryStr { get; set; }
+        public string CountryCode { get; set; }
+        public string Slug { get; set; }
+        public int NewConfirmed { get; set; }
+        public int TotalConfirmed { get; set; }
+        public int NewDeaths { get; set; }
+        public int TotalDeaths { get; set; }
+        public int NewRecovered { get; set; }
+        public int TotalRecovered { get; set; }
+        public DateTime Date { get; set; }
+    }
+
+    public class Root
+    {
+        public Global Global { get; set; }
+        public List<Country> Countries { get; set; }
+        public DateTime Date { get; set; }
+    }
+}
---
+++ COVID-test/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace COVID_test
+{
+    class Program
+    {
+        private static readonly HttpClient client = new HttpClient();
+
+        static async Task Main(string[] args)
+        {
+            var repositories = await ProcessRepositories();
+
+            foreach (var repo in repositories.Countries)
+            {
+                Console.WriteLine(repo.CountryStr);
+                Console.WriteLine($"Total deaths: {repo.TotalDeaths}");
+                Console.WriteLine($"New deaths: {repo.NewDeaths}");
+                Console.WriteLine(repo.Date);
+                Console.WriteLine("========================");
+            }
+        }
+
+        private static async Task<Root> ProcessRepositories()
+        {
+            client.DefaultRequestHeaders.Accept.Clear();
+            client.DefaultRequestHeaders.Accept.Add(
+                new MediaTypeWithQualityHeaderValue("application/json"));
+
+
+            var streamTask = client.GetStreamAsync("https://api.covid19api.com/summary");
+
+            var myDeserializedClass = await JsonSerializer.DeserializeAsync<Root>(await streamTask);
+
+            return myDeserializedClass;
+        }
+    }
+}
